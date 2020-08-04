ee22b93d1186147d11fbcbf56aa21ac3485b3b63
ViktorB <basharkevichv@gmail.com>
2020-08-04 13:51:58 -0400

    Setup react on frontend w/ babel and removed added node_modules in .gitignore

---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Testing</h1>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));

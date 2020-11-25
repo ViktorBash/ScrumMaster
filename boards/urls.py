4512af284436ef97d77caf4c965540c16f0ded46
ViktorB <basharkevichv@gmail.com>
2020-11-25 15:11:16 -0500

    Fixed bug where Board endpoints didn't work because or URl routing. Put path api/board/<str:url>/ at the bottom to prevent other board requests going to this path first

--- boards/urls.py
+++ boards/urls.py
@@ -3,8 +3,8 @@ from .api import BoardCreate, BoardInfo, BoardList, SharedUserCreate, SharedUser
 
 urlpatterns = [
     path('api/board/create/', BoardCreate.as_view()),
-    path('api/board/<str:url>/', BoardInfo.as_view()),  # For retrieve, update and delete (Aka GET, PUT, DELETE)
     path('api/board/list/', BoardList.as_view()),  # get list of shared boards and owned boards
+    path('api/board/<str:url>/', BoardInfo.as_view()),  # For retrieve, update and delete (Aka GET, PUT, DELETE)
     path('api/shareduser/create/', SharedUserCreate.as_view()),
     path('api/shareduser/delete/', SharedUserDelete.as_view()),
     path('api/task/create/', TaskCreate.as_view()),

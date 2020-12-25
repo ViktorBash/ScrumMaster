d50d055311a76ccf63b8713cc253b7bc8cae675a
ViktorB <basharkevichv@gmail.com>
2020-12-25 00:52:18 -0500

    Worked on Shareduser component

--- frontend/src/components/specific-board/SharedUsers.js
+++ frontend/src/components/specific-board/SharedUsers.js
@@ -3,12 +3,12 @@ import React, { Component, Fragment } from "react";
 export class SharedUsers extends Component {
     render() {
         // shared_users: Prop that is all of the shared users in a list
-        console.log(this.props.shared_users)
         return(
             <Fragment>
                 <ul className="list-group">
                     {this.props.shared_users.map(shared_user => (
-                        <li className="list-group-item">{shared_user.username + ": " + shared_user.email}</li>
+                        <li key={shared_user.id} className="list-group-item">
+                            {shared_user.username + ": " + shared_user.email} <button className="btn btn-danger btn-sm float-right">Delete</button></li>
                     ))}
                 </ul>
 
